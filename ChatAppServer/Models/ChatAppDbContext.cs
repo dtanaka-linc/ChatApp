b4423630a5b41399d0d3b555997d34916d037d19
KanadeSawada <k.sawada@lincraft.co.jp>
2020-04-03 11:38:27 +0900

    Created User and ChatLogs

---
+++ ChatAppServer/Models/ChatAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatAppServer.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ChatAppDbContext : DbContext {
+        //public ChatAppDbContext() : base("ChatAppDbContext") { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ChatLog> ChatLogs { get; set; }
+    }
+}
