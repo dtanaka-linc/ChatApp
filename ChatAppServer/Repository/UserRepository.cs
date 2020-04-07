6358908b3ceaab2cf14c0380904d0157da1b6234
KanadeSawada <k.sawada@lincraft.co.jp>
2020-04-07 17:14:52 +0900

    メソッド名の修正、XMLコメントの追加

--- ChatAppServer/Repository/UserRepository.cs
+++ ChatAppServer/Repository/UserRepository.cs
@@ -6,7 +6,10 @@ using ChatAppServer.Models;
 
 namespace ChatAppServer.Repository
 {
-    class UserRepository
+    /// <summary>
+    /// Userエンティティにアクセスし新規登録や認証を行うメソッド群です。
+    /// </summary>
+    public class UserRepository
     {
         private ChatAppDbContext DbContext { get; set; }
 
@@ -16,12 +19,17 @@ namespace ChatAppServer.Repository
             this.DbContext = dbContext;
         }
 
-        //新規ユーザー登録
-        public Boolean CreateUser(string name, string password)
+        /// <summary>
+        /// 新規ユーザー登録を行います。※名前確認部分の切り離しはこれからおこないます
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public User CreateUser(string name, string password)
         {
-            var isUserName = IsUserName(name);
+            var isUserName = ExistsUserName(name);
 
-            if (isUserName  == true)
+            if (isUserName)
             {
                 var user = new User()
                 {
@@ -30,22 +38,30 @@ namespace ChatAppServer.Repository
                 };
                 DbContext.Users.Add(user);
                 DbContext.SaveChanges();
-                return true;
-            } else
+                return user;
+            } 
+            else
             {
                 return false;
             }
         }
 
-        //ユーザー名検索
+
+        /// <summary>
+        /// ユーザー名で検索する
+        /// </summary>
         public IQueryable<User> FindByUserName(string userName)
         {
             return this.DbContext.Users.Where(r => r.Name == userName);
         }
 
 
-        //ユーザー名が存在するかどうか
-        public Boolean IsUserName(string userName)
+        /// <summary>
+        /// 入力されたユーザー名と同名のレコードが存在するかどうか確認する
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public bool ExistsUserName(string userName)
         {
             return this.DbContext.Users.Any(r => r.Name == userName);
         }
