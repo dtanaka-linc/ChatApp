a9a81d886e66f0d1e8609490a4dead9ce28296c0
KanadeSawada <k.sawada@lincraft.co.jp>
2020-04-07 10:53:57 +0900

    ユーザー名検索メソッド追加

--- ChatAppServer/Repository/UserRepository.cs
+++ ChatAppServer/Repository/UserRepository.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using ChatAppServer.Models;
 
 namespace ChatAppServer.Repository
 {
     class UserRepository
     {
+        private ChatAppDbContext DbContext { get; set; }
+
+        //コンストラクタ
+        public UserRepository(ChatAppDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        //新規ユーザー登録
+        public void CreateUser(User user)
+        {
+
+        }
+
+        //ユーザー名検索
+        public IQueryable<User> FindByUserName(string userName)
+        {
+            return this.DbContext.Users.Where(r => r.Name == userName);
+        }
     }
 }
