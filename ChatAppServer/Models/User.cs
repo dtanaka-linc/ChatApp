0af8e1ed80402df83120c33e6ceae0d0557bbc92
KanadeSawada <k.sawada@lincraft.co.jp>
2020-04-07 17:29:43 +0900

    UserモデルのNameにユニーク制約を追加

--- ChatAppServer/Models/User.cs
+++ ChatAppServer/Models/User.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ChatAppServer.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Index(IsUnique = true)]
         public string Name { get; set; }
         public string Password { get; set; }
     }
