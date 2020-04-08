8c9749f8194d5538b52719d176f5ac1e2ed7a190
KanadeSawada <k.sawada@lincraft.co.jp>
2020-04-08 18:08:43 +0900

    ChatLogRepositoryクラスの作成とCreateChatLogメソッドの追加

---
+++ ChatAppServer/Migrations/202004080800426_AddColumnsToUsers.cs
@@ -0,0 +1,20 @@
+﻿namespace ChatAppServer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnsToUsers : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "UpdatedAt", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Users", "CreatedAt", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "CreatedAt");
+            DropColumn("dbo.Users", "UpdatedAt");
+        }
+    }
+}
---
+++ ChatAppServer/Migrations/202004080803104_AddColumnsToChatLogs.cs
@@ -0,0 +1,20 @@
+﻿namespace ChatAppServer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnsToChatLogs : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ChatLogs", "UpdatedAt", c => c.DateTime(nullable: false));
+            AddColumn("dbo.ChatLogs", "CreatedAt", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ChatLogs", "CreatedAt");
+            DropColumn("dbo.ChatLogs", "UpdatedAt");
+        }
+    }
+}
