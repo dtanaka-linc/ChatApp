78c87382e2474a32b8e97843cd3276b866ce05bc
KanadeSawada <k.sawada@lincraft.co.jp>
2020-04-14 12:48:54 +0900

    add Untraked file

---
+++ ChatAppLibrary/Telegram/Header.cs
@@ -0,0 +1,20 @@
+﻿namespace ChatAppLibrary.Telegram
+{
+    /// <summary>
+    /// 各処理に共通する部分
+    /// </summary>
+    public class Header
+    {
+        /// <summary>
+        /// 処理種別
+        /// </summary>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        public string UserName { get; set; }
+
+
+    }
+}
